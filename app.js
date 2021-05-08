f93a7d1191b58f1ca6ea2a2fd017ab7e842c623c
iam-jmismail <jmismail628@gmail.com>
2021-05-08 23:17:03 +0530

    PORT Changed to heroku dynammics

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static('public'))
 
-app.listen(3000, () => console.log('App is listening'))
+app.listen(port, () => console.log(`App is listening ${port}`))
